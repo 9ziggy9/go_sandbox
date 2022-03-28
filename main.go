fbad09b2fecef1d6766a7534560eeb0d318a19cf
Ziggy <davidarogers@protonmail.com>
2022-03-28 19:26:45 +0000

    migrating to laptop

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	conferenceName := "Go Conference"
+
+	fmt.Printf("Welcome to %s booking application\n", conferenceName)
+	fmt.Println("Get your tickets here to attend")
+}
